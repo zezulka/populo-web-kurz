6f51c34eb2d3f4f71d4314fa4f29f8c1b003cb15
Miloslav Žežulka <miloslav.zezulka@cleverfarm.ag>
2022-11-23 17:37:05 +0100

    add lecture notes

--- src/index.php
+++ src/index.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- tímto elementem načítáme kaskádové styly, které používáme v projektu -->
     <link rel="stylesheet" href="simple.css">
+    <!-- tímto elementem načítáme externí písmo -->
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster">
     <title>TEXPRA, s.r.o.</title>
 </head>
@@ -17,6 +19,9 @@
                     </ul>
                 </div>
                 <nav class="navbar">
+                    <!-- toto je hypertextový odkaz, kterým se přesměruji na google (v nové záložce prohlížeče, výchozí chování je, že se přesměřováváme přímo na stránce) -->
+                    <a href="http://google.com" target="_blank">Google</a>
+                    <!-- zde se nepřesměrováváme na jiný dokument, ale "říkáme" iframu, aby načetl "welcome.html" -->
                     <a href="welcome.html" target="content">Domů</a>
                     <div class="dropdown">
                         <button class="dropbtn">Obchod</button>
@@ -27,6 +32,7 @@
                     </div>
                     <a href="#">Kontakt</a>
                 </nav>
+                <!-- frame má rozměry 800x600 px, jeho identifikátor je "content" a výchozí stránka, která se má zobrazit, je soubor "about.html" -->
                 <iframe src="about.html" width="800px" height="600px" id="content" name="content" frameBorder="0"></iframe>
             </div>
             <?php require("copyright.php") ?>
